404bab48a6cd1e366c1e40dde222b1ce3bee307f
Tom Cubie <tom@radxa.com>
2015-04-29 16:10:49 +0800

    add local repository configs

--- rabian-build.sh
+++ rabian-build.sh
@@ -4,27 +4,40 @@ BOARD=$1
 TODAY=$(date +"%y%m%d")
 
 if [ -z ${BOARD} ]; then
-	echo "$0 board"
+	echo "$0 board [local]"
 	exit 1
 fi
 
 mkdir -p ${TODAY} && cd ${TODAY}
 
+
+if [ "local" = "$2" ]; then
+	LB_URL="git@x1:radxa-lb.git"
+	BSP_URL="git@x1:github/rock-bsp.git"
+
+else
+	LB_URL="git@git.radxa.com:x1/radxa-lb.git"
+	BSP_URL="git@github.com:radxa/rock-bsp.git"
+fi
+
 if [ ! -d radxa-lb/.git ]; then
-	git clone git@git.radxa.com:x1/radxa-lb.git
+	git clone ${LB_URL}
 fi
 
 if [ ! -d rock-bsp/.git ]; then
-	git clone git@github.com:radxa/rock-bsp.git
+	git clone ${BSP_URL}
 fi
 
-if [ "local" = $2 ]; then
-	cat ../bootstrap_local > radxa-lb/${BOARD}/bootstrap
+if [ "local" = "$2" ]; then
+	cp -vf ../bootstrap_local radxa-lb/${BOARD}/bootstrap
 	echo "deb http://172.168.1.3/apt.radxa.us/rabian-stable/ jessie main" > radxa-lb/${BOARD}/archives/apt-radxa-us.list
-
+	cp -vf ../defconfig_local rock-bsp/configs/defconfig
+	cp -vf ../${BOARD}_config_local rock-bsp/configs/${BOARD}_config
 fi
 
-cd radxa-lb && make clean && make $BOARD && cd -
+if [ ! -e radxa-lb/.${BOARD}_lb_done ]; then
+	cd radxa-lb && make clean && make $BOARD && touch ./.${BOARD}_lb_done && cd -
+fi
 
 IMAGE=$(basename radxa-lb/rabian_${BOARD}_*.ext4)
 
