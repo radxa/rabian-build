dcffefc1ca81317ddebbb22366665688fe55ab9a
Tom Cubie <tom@radxa.com>
2015-05-08 15:54:12 +0800

    use github repository

--- rabian-build.sh
+++ rabian-build.sh
@@ -1,24 +1,23 @@
 #!/bin/sh -e
 
 BOARD=$1
+OUT=$2
+
 TODAY=$(date +"%y%m%d")
 
+LB_URL="https://github.com/radxa/radxa-lb.git"
+BSP_URL="https://github.com/radxa/rock-bsp.git"
+
 if [ -z ${BOARD} ]; then
-	echo "$0 board [local]"
+	echo "$0 board [output]"
 	exit 1
 fi
 
-mkdir -p ${TODAY} && cd ${TODAY}
-
-
-if [ "local" = "$2" ]; then
-	LB_URL="git@x1:radxa-lb.git"
-	BSP_URL="git@x1:github/rock-bsp.git"
-
-else
-	LB_URL="git@git.radxa.com:x1/radxa-lb.git"
-	BSP_URL="git@github.com:radxa/rock-bsp.git"
+if [ ! -z ${OUT} ]; then
+	mkdir -p ${OUT}/${BOARD}/${TODAY}
 fi
+
+mkdir -p ${TODAY} && cd ${TODAY}
 
 if [ ! -d radxa-lb/.git ]; then
 	git clone ${LB_URL}
@@ -28,9 +27,9 @@ if [ ! -d rock-bsp/.git ]; then
 	git clone ${BSP_URL}
 fi
 
-if [ "local" = "$2" ]; then
+if [ -e ../.local ]; then
 	cp -vf ../bootstrap_local radxa-lb/${BOARD}/bootstrap
-	echo "deb http://172.168.1.3/apt.radxa.us/rabian-stable/ jessie main" > radxa-lb/${BOARD}/archives/apt-radxa-us.list
+	cp -vf ../apt-radxa-us.list.local radxa-lb/${BOARD}/archives/apt-radxa-us.list
 	cp -vf ../defconfig_local rock-bsp/configs/defconfig
 	cp -vf ../${BOARD}_config_local rock-bsp/configs/${BOARD}_config
 fi
@@ -45,4 +44,4 @@ cp -vf radxa-lb/$IMAGE rock-bsp/rootfs/
 
 echo "BOARD_ROOTFS=${IMAGE}" >> rock-bsp/configs/${BOARD}_config
 
-cd rock-bsp && ./config.sh $BOARD && make && cd -
+cd rock-bsp && ./config.sh $BOARD && make && mv boards/${BOARD}/rockdev ${OUT}/${BOARD}/${TODAY}/ && cd -
